d13d96e66cadce114d6fcc70c902d40a851a19f2
gift56 <efeasiughu@gmail.com>
2023-10-16 18:16:11 -0400

    working on the dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,10 @@
   <main
     class="flex items-start justify-start w-full h-screen overflow-hidden gap-6 p-6"
   >
-    <div class="flex-shrink"></div>
-    <div class="flex-grow"></div>
-    <div class="flex-shrink"></div>
+    <div class="flex-shrink">
+      
+    </div>
+    <div class="flex-grow">djdj</div>
+    <div class="flex-shrink">World</div>
   </main>
 </template>
