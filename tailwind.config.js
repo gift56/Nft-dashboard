d65f75d48f060120a982b1edb505529a9b0fb858
gift56 <efeasiughu@gmail.com>
2023-10-16 17:12:10 -0400

    setting up variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
         bodyColor: "#0B0D21",
         sidebarhoverColor: "#232966",
         inputBorderColor: "#212554",
+        darkColor: "#161A42",
+        primary: "#1FA2FF",
+        primary2: "#5EEAD4",
       },
       backgroundImage: {
         inputBg:
           "linear-gradient(162deg, #161A42 22.61%, rgba(22, 26, 66, 0.00) 118.29%)",
+        grayBg: "linear-gradient(90deg, #9796F0 0%, #FBC7D4 100%)",
+        textBg:
+          "linear-gradient(89deg, #BAC1FF -0.98%, #63FEFE 54.08%, #FF3EEC 112.69%)",
       },
     },
   },
