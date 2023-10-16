4e35767e0c636c8f396dfe397d1280078224aadf
gift56 <efeasiughu@gmail.com>
2023-10-16 17:33:33 -0400

    working on the dashboard

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,10 +1,31 @@
+import {
+  ActivityIcon,
+  BarIcon,
+  BuyIcon,
+  MessageIcon,
+  TimeIcon,
+} from "../assets";
 
-
-
-// export const leftSideBar = [
-//   {
-//     img: ,
-//   },
-// ];
+export const leftSideBar = [
+  {
+    firstIcons: [
+      {
+        iconImage: BarIcon,
+      },
+      {
+        iconImage: MessageIcon,
+      },
+      {
+        iconImage: BuyIcon,
+      },
+      {
+        iconImage: ActivityIcon,
+      },
+      {
+        iconImage: TimeIcon,
+      },
+    ],
+  },
+];
 
 export const topSellers = [{}];
