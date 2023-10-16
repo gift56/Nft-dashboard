0095a714f65307bf5f19af3afa562a56165f9d27
gift56 <efeasiughu@gmail.com>
2023-10-16 18:30:18 -0400

    working on the dashboard

--- src/components/LeftSideBar.vue
+++ src/components/LeftSideBar.vue
@@ -5,6 +5,11 @@
     <div class="w-full flex items-center justify-center py-4">
       <img src="@/assets/image/logo.png" alt="logo" />
     </div>
+    <div class="flex flex-col items-center justify-center w-full gap-8">
+      <div
+        class="w-full relative before:content-[''] before:absolute before:bottom-0 before:w-[80%] before:h-[3px] before:left-[8px] before:bg-[#2E357B] pb-7 flex flex-col"
+      ></div>
+    </div>
   </div>
 </template>
 
