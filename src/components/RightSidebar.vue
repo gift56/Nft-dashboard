a89d3804083ee5109080b0bfe3e429c65893a6b3
gift56 <efeasiughu@gmail.com>
2023-10-16 19:25:16 -0400

    working on the dashboard

--- src/components/RightSidebar.vue
+++ src/components/RightSidebar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>RightSideBar</div>
+  <div class="w-[268px] bg-inputBorderColor border border-[#075985] rounded-2xl h-[94vh] overflow-hidden hover:overflow-y-auto hover:overflow-x-hidden">
+    RightSideBar
+  </div>
 </template>
 
 <script>
