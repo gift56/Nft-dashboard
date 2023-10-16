692def809d392de8199045133043822cd6d9c7c9
gift56 <efeasiughu@gmail.com>
2023-10-16 17:31:27 -0400

    working on the dashboard

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,8 @@ import MessageIcon from "./icons/messageIcon.png";
 import BuyIcon from "./icons/buyIcon.png";
 import ActivityIcon from "./icons/activityIcon.png";
 import TimeIcon from "./icons/timeIcon.png";
+import WalletIcon from "./icons/walletIcon.png";
+import FriendsIcon from "./icons/friendsIcon.png";
+import SettingsIcon from "./icons/settingsIcon.png";
 
 export { BarIcon };
