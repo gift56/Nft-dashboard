1ca388fc536c7ca83c131170a498be417a51f02b
gift56 <efeasiughu@gmail.com>
2023-10-16 17:05:35 -0400

    importing images and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        
-      }
+      colors: {
+        bodyColor: "#0B0D21",
+        sidebarhoverColor: "#232966",
+        inputBorderColor: "#212554",
+      },
+      backgroundImage: {
+        inputBg:
+          "linear-gradient(162deg, #161A42 22.61%, rgba(22, 26, 66, 0.00) 118.29%)",
+      },
     },
   },
   plugins: [],
