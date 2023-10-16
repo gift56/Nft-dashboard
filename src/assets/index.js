327e10af36ce0dc692268c91494a8d68685ac3c4
gift56 <efeasiughu@gmail.com>
2023-10-16 17:29:45 -0400

    working on the dashboard

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import BarIcon from "./icons/barIcon.png";
+import MessageIcon from "./icons/messageIcon.png";
+import BuyIcon from "./icons/buyIcon.png";
+import ActivityIcon from "./icons/activityIcon.png";
+import TimeIcon from "./icons/timeIcon.png";
+
+export { BarIcon };
