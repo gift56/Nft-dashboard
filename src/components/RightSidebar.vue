47efdb189751f46f58fc9892dd64e9871fad04f2
gift56 <efeasiughu@gmail.com>
2023-10-16 19:26:33 -0400

    working on the dashboard

--- src/components/RightSidebar.vue
+++ src/components/RightSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-[268px] bg-inputBorderColor border border-[#075985] rounded-2xl h-[94vh] overflow-hidden hover:overflow-y-auto hover:overflow-x-hidden flex flex-col items-center justify-center gap-8 relative"
+    class="w-[268px] bg-inputBorderColor border-2 border-[#075985] rounded-2xl h-[94vh] overflow-hidden hover:overflow-y-auto hover:overflow-x-hidden flex flex-col items-center justify-start gap-8 relative p-4"
   >
     RightSideBar
   </div>
