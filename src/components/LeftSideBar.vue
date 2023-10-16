b4a42f495911c5a8e266ca0c8fa01664e1960007
gift56 <efeasiughu@gmail.com>
2023-10-16 18:46:27 -0400

    working on the dashboard

--- src/components/LeftSideBar.vue
+++ src/components/LeftSideBar.vue
@@ -7,12 +7,15 @@
     </div>
     <div class="flex flex-col items-center justify-center w-full gap-8">
       <div
-        class="w-full relative before:content-[''] before:absolute before:bottom-0 before:w-[80%] before:h-[3px] before:left-[8px] before:bg-[#2E357B] pb-7 flex flex-col gap-6"
+        class="w-full relative before:content-[''] before:absolute before:bottom-0 before:w-[80%] before:h-[3px] before:left-[8px] before:bg-[#2E357B] pb-7 flex flex-col items-center justify-center gap-6"
       >
         <div
           v-for="icon in leftSideBarData"
-          class="bg-sidebarhoverColor py-2 w-full"
-        ></div>
+          :key="icon.iconImage"
+          class="first:bg-sidebarhoverColor w-full flex items-center justify-center h-10 hover:bg-sidebarhoverColor transition-all duration-300 cursor-pointer"
+        >
+          <img :src="icon.iconImage" alt="icons" />
+        </div>
       </div>
     </div>
   </div>
@@ -23,5 +26,10 @@ import { leftSideBarData } from "../utils/constant";
 
 export default {
   name: "LeftSideBar",
+  data() {
+    return {
+      leftSideBarData,
+    };
+  },
 };
 </script>
