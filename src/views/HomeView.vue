1fb00028d94dce7060ea42e6aee56fad1d6c46d8
gift56 <efeasiughu@gmail.com>
2023-10-16 18:14:27 -0400

    working on the dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <main class="flex items-start justify-start w-full h-screen overflow-hidden gap-6">
-
+  <main
+    class="flex items-start justify-start w-full h-screen overflow-hidden gap-6 p-6"
+  >
+    hello
   </main>
 </template>
