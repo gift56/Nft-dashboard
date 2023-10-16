4cf9e48291c1c7556707016384c46681258b2465
gift56 <efeasiughu@gmail.com>
2023-10-16 19:49:27 -0400

    working on the dashboard

--- src/components/RightSidebar.vue
+++ src/components/RightSidebar.vue
@@ -11,9 +11,10 @@
         alt="user_avatar"
         class="border-2 rounded-full border-[#fc6767]"
       />
-      <h4 class="text-2xl font-semibold font-quicksand">Musfiqur Rahman</h4>
+      <h4 class="text-2xl font-semibold font-quicksand relative z-10">Musfiqur Rahman</h4>
       <div class="w-12 h-12 bg-ballsBg rounded-full absolute top-[5%] right-[-15%]"></div>
-      <div class="w-16 h-16 bg-balls2Bg rounded-full"></div>
+      <div class="w-16 h-16 bg-balls2Bg rounded-full absolute top-[20%] right-[-25%] z-10"></div>
+      <div class="w-16 h-16 bg-[#FFA336] rounded-full absolute top-[25%] right-[-25%] blur-[39.5px]"></div>
       <div class="w-16 h-16 bg-balls3Bg rounded-full"></div>
       <div class="w-8 h-16 bg-balls4Bg rounded-full"></div>
       <div class="w-5 h-5 bg-smallBallsBg rounded-full"></div>
