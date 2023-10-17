a35f1dc3c4bd178c7674ca4f62493a8c2028102c
gift56 <efeasiughu@gmail.com>
2023-10-17 08:14:26 -0400

    working on the main dashboard

--- src/components/Sellers.vue
+++ src/components/Sellers.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col w-full items-start justify-start gap-5">
-    
+    <h2 class="text-2xl font-bold font-quicksand text-[#6EE7B7] select-none">
+      ⭐ Top Sellers
+    </h2>
   </div>
 </template>
 
