73bb38d091932ecd2b59901fe13daf2ce1d49887
gift56 <efeasiughu@gmail.com>
2023-10-16 23:44:21 -0400

    working on the main dashboard

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Popular</div>
+  <div class="flex flex-col w-full items-start justify-start gap-5">
+    <div class="w-full flex items-center justify-between gap-4">
+      
+    </div>
+
+  </div>
 </template>
 
 <script>
