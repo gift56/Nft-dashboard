b1f9b581de2e1f18c8a8ed4d6009544cc446de42
gift56 <efeasiughu@gmail.com>
2023-10-16 21:09:19 -0400

    working on the main dashboard

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <template>
   <header class="w-full flex items-center justify-between gap-6">
-    <div class="flex items-center justify-start gap-5 w-[400px] h-14 rounded-2xl border-2 border-inputBorderColor bg-inputBg"></div>
+    <div
+      class="flex items-center justify-start gap-5 w-[400px] h-14 rounded-2xl border-2 border-inputBorderColor bg-inputBg px-4"
+    >
+      <img src="@/assets/icons/searchIcon.png" alt="searchIcon" />
+      <input type="text" placeholder="Search by creator or collection" class="h-full w-full bg-transparent outline-none text-base font-normal" />
+    </div>
   </header>
 </template>
 
