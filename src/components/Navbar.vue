544ce6589558e481347f151ec772ca1ddb688275
gift56 <efeasiughu@gmail.com>
2023-10-16 21:11:42 -0400

    working on the main dashboard

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,17 @@
       class="flex items-center justify-start gap-5 w-[400px] h-14 rounded-2xl border-2 border-inputBorderColor bg-inputBg px-4"
     >
       <img src="@/assets/icons/searchIcon.png" alt="searchIcon" />
-      <input type="text" placeholder="Search by creator or collection" class="h-full w-full bg-transparent outline-none text-base font-normal" />
+      <input
+        type="text"
+        placeholder="Search by creator or collection"
+        class="h-full w-full bg-transparent outline-none text-base font-normal"
+      />
+    </div>
+    <div
+      class="w-fit h-14 rounded-2xl border-2 border-inputBorderColor bg-inputBg px-4 flex items-center justify-start gap-3"
+    >
+      <img src="../assets/icons/ethImage.png" alt="ethImage" />
+      <h4 class="text-base font-medium">3,25 ETH</h4>
     </div>
   </header>
 </template>
