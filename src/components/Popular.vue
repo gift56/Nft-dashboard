04a22db37f56c6543ec234e0069bf891c6d80656
gift56 <efeasiughu@gmail.com>
2023-10-16 23:45:24 -0400

    working on the main dashboard

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="flex flex-col w-full items-start justify-start gap-5">
     <div class="w-full flex items-center justify-between gap-4">
-      
+      <h2 class="text-3xl font-bold font-quicksand text-[#E2E4E9]">
+        Popular NFT’s Live Auction
+      </h2>
     </div>
-
   </div>
 </template>
 
