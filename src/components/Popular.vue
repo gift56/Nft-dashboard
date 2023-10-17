5413085971ec2f9aafd5d748213c74e4059ad166
gift56 <efeasiughu@gmail.com>
2023-10-16 23:47:01 -0400

    working on the main dashboard

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -4,6 +4,10 @@
       <h2 class="text-3xl font-bold font-quicksand text-[#E2E4E9]">
         Popular NFT’s Live Auction
       </h2>
+      <div class="flex items-center justify-end gap-2 cursor-pointer">
+        <span class="text-sm font-bold text-[#5EEAD4] font-quicksand">Show More</span>
+        <img src="../assets/icons/rightArrowIcon.png" alt="rightArrow" />
+      </div>
     </div>
   </div>
 </template>
