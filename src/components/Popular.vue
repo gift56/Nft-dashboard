f2b487594fbe9d3deaf257f5fa59531b10419309
gift56 <efeasiughu@gmail.com>
2023-10-16 23:48:38 -0400

    working on the main dashboard

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -5,8 +5,18 @@
         Popular NFT’s Live Auction
       </h2>
       <div class="flex items-center justify-end gap-2 cursor-pointer">
-        <span class="text-sm font-bold text-[#5EEAD4] font-quicksand">Show More</span>
+        <span class="text-sm font-bold text-[#5EEAD4] font-quicksand"
+          >Show More</span
+        >
         <img src="../assets/icons/rightArrowIcon.png" alt="rightArrow" />
+      </div>
+    </div>
+    <div class="w-full flex items-center justify-between gap-4">
+      <div class="flex-1 relative">
+        <img src="../assets/image/nftImage1.png" alt="nft1" />
+      </div>
+      <div class="flex-1 relative">
+        <img src="../assets/image/nftImage2.png" alt="nft2" />
       </div>
     </div>
   </div>
