84d40fb5b2410a907d4457bfe74d940119f76c9d
gift56 <efeasiughu@gmail.com>
2023-10-16 23:51:26 -0400

    working on the main dashboard

--- src/components/Popular.vue
+++ src/components/Popular.vue
@@ -12,11 +12,11 @@
       </div>
     </div>
     <div class="w-full flex items-center justify-between gap-4">
-      <div class="flex-1 relative">
-        <img src="../assets/image/nftImage1.png" alt="nft1" />
+      <div class="flex-1 relative h-[280px]">
+        <img src="../assets/image/nftImage1.png" alt="nft1" class="h-full rounded-3xl" />
       </div>
-      <div class="flex-1 relative">
-        <img src="../assets/image/nftImage2.png" alt="nft2" />
+      <div class="flex-1 relative h-[280px]">
+        <img src="../assets/image/nftImage2.png" alt="nft2" class="h-full rounded-3xl" />
       </div>
     </div>
   </div>
