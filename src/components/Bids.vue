74702939ffdc1e545bc328574df33bc57f1a424c
gift56 <efeasiughu@gmail.com>
2023-10-17 08:36:03 -0400

    working on bids section

--- src/components/Bids.vue
+++ src/components/Bids.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Bids</div>
+  <div class="flex flex-col w-full items-start justify-start gap-5">
+    <h2 class="text-2xl font-semibold font-quicksand text-[#6EE7B7] select-none">
+      🔥 Hot Bids
+    </h2>
+    <div class="w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+      
+    </div>
+  </div>
 </template>
 
 <script>
