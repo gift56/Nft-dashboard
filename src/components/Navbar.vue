0b623c3529b726e40b130b70e708b40f3a7cd3f3
gift56 <efeasiughu@gmail.com>
2023-10-16 21:07:13 -0400

    working on the main dashboard

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="w-full flex items-center justify-between gap-6">
-
+    <div class="flex items-center justify-start gap-5 w-[400px] h-14 rounded-2xl border-2 border-inputBorderColor bg-inputBg"></div>
   </header>
 </template>
 
